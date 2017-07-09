aaa7b0003f5485e62abd226707f9759f7778a1d4
Are Strom <are.strom@gmail.com>
2017-07-09 13:34:39 -0700

    Added pg_terminate, test_functions.R, doc examples.

--- R/connect.R
+++ R/connect.R
@@ -19,7 +19,7 @@ pg_host <- function(host_label) {
   Sys.getenv(host_label)
 }
 
-#' @title Create a connection object to a database on a PostgreSQL server
+#' @title Create connection object to database on a PostgreSQL server
 #'
 #' @description Creates a connection object that can be used to connect to a
 #'   specific database an a PostgreSQL database server. This function
@@ -32,17 +32,16 @@ pg_host <- function(host_label) {
 #'   \code{host_label, user_label, pw_label} are needed to locate your
 #'   connection credentials in your \code{.Renviron} file.
 #'
-#' @section Warning: Remember to \emph{always close} your connection after you
-#'   are done. See example below.
+#' @section Warning:
+#' Remember to \emph{always close} your connection after you are done. See
+#' example below.
+#'
 #' @rdname pg_con
 #' @param dbname a database on a PostgreSQL server
-#' @param host_label a string label to identify the server host in the .Renviron
-#'   file
-#' @param user_label a string label to identify the server username in the
-#'   .Renviron file
-#' @param pw_label a string label to identify the server password in the
-#'   .Renviron file
-#' @param port a port string used to connect to the database. Defaults to '5432'
+#' @param host_label a string label to identify the server host
+#' @param user_label a string label to identify the server username
+#' @param pw_label a string label to identify the server password
+#' @param port a port string used to connect to the database
 #' @return A connection object to a database located on a PostgreSQL server
 #' @examples
 #' \dontrun{
@@ -69,4 +68,71 @@ pg_con = function(host_label, dbname, user_label, pw_label, port = '5432') {
     user = pg_user(user_label),
     password = pg_pw(pw_label))
   con
+}
+
+#' @title Terminate all connections to a database
+#'
+#' @description Terminates all connections to a database in case administrative
+#'   tasks require all connections to be closed. This function \strong{requires}
+#'   that you have administrative permissions to the database and that you
+#'   supply superuser credentials to the \code{pg_con()} function.
+#'
+#' @section Warning:
+#' If pointing this function to any server other than your localhost, verify
+#' first that you are not disrupting any important connections. You may
+#' seriously mess up the work of others unless you understand what you are
+#' doing.
+#'
+#' @rdname pg_terminate_backend
+#' @param dbname a database on a PostgreSQL server
+#' @param host_label a string label to identify the server host
+#' @param user_label a string label to identify the server username
+#' @param pw_label a string label to identify the server password
+#' @param port a port string used to connect to the database
+#' @return A boolean TRUE or FALSE
+#' @examples
+#' \dontrun{
+#' # Check for open connections to database
+#' db_con_local = pg_con(
+#'   host_label = "my_pg_host_local",
+#'   dbname = "sport_sampling",
+#'   user_label = "my_pg_username_local",
+#'   pw_label = "my_pg_password_local")
+#' open_connections = DBI::dbGetQuery(db_con_local,
+#'                                    "SELECT * FROM pg_stat_activity")
+#' DBI::dbDisconnect(db_con_local)
+#'
+#' # Inspect
+#' open_connections
+#'
+#' # Terminate all connections to a PostgreSQL database on localhost
+#' pg_message = pg_terminate_backend(
+#'   host_label = "my_pg_host_local",
+#'   dbname = "sport_sampling",
+#'   user_label = "my_pg_super_username_local",
+#'   pw_label = "my_pg_super_password_local")
+#'
+#' # Inspect. Should return: [1] TRUE
+#' pg_message
+#'
+#' # Verify in pgAdmin that all connections, except the current, are closed.
+#' }
+#' @export
+pg_terminate_backend = function(host_label,
+                                dbname,
+                                user_label,
+                                pw_label,
+                                port = '5432') {
+  db_con = pg_con(host_label = host_label,
+                  dbname = dbname,
+                  user_label = user_label,
+                  pw_label = pw_label,
+                  port = port)
+  dbGetQuery(db_con,
+             paste(sep = "",
+                   "SELECT pg_terminate_backend(pg_stat_activity.pid) ",
+                   "FROM pg_stat_activity ",
+                   "WHERE pg_stat_activity.datname = '", dbname, "' ",
+                   "AND pid <> pg_backend_pid()"))
+  dbDisconnect(db_con)
 }
